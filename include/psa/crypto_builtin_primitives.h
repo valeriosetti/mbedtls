082aa068978eb1a31ac63301de1514f10b8222dd
Valerio Setti <valerio.setti@nordicsemi.no>
2023-09-03 08:45:44 +0200

    crypto: fix size of structures
    Signed-off-by: Valerio Setti <valerio.setti@nordicsemi.no>

--- include/psa/crypto_builtin_primitives.h
+++ include/psa/crypto_builtin_primitives.h
@@ -60,21 +60,28 @@ typedef struct {
     psa_algorithm_t MBEDTLS_PRIVATE(alg);
     union {
         unsigned dummy; /* Make the union non-empty even with no supported algorithms. */
-#if defined(MBEDTLS_PSA_BUILTIN_ALG_MD5)
+#if defined(MBEDTLS_PSA_BUILTIN_ALG_MD5) || \
+        (defined(MBEDTLS_PSA_CRYPTO_CLIENT) && defined(PSA_WANT_ALG_MD5))
         mbedtls_md5_context md5;
 #endif
-#if defined(MBEDTLS_PSA_BUILTIN_ALG_RIPEMD160)
+#if defined(MBEDTLS_PSA_BUILTIN_ALG_RIPEMD160) || \
+        (defined(MBEDTLS_PSA_CRYPTO_CLIENT) && defined(PSA_WANT_ALG_RIPEMD160))
         mbedtls_ripemd160_context ripemd160;
 #endif
-#if defined(MBEDTLS_PSA_BUILTIN_ALG_SHA_1)
+#if defined(MBEDTLS_PSA_BUILTIN_ALG_SHA_1) || \
+        (defined(MBEDTLS_PSA_CRYPTO_CLIENT) && defined(PSA_WANT_ALG_SHA_1))
         mbedtls_sha1_context sha1;
 #endif
 #if defined(MBEDTLS_PSA_BUILTIN_ALG_SHA_256) || \
-        defined(MBEDTLS_PSA_BUILTIN_ALG_SHA_224)
+        (defined(MBEDTLS_PSA_CRYPTO_CLIENT) && defined(PSA_WANT_ALG_SHA_256)) || \
+        defined(MBEDTLS_PSA_BUILTIN_ALG_SHA_224) || \
+        (defined(MBEDTLS_PSA_CRYPTO_CLIENT) && defined(PSA_WANT_ALG_224))
         mbedtls_sha256_context sha256;
 #endif
 #if defined(MBEDTLS_PSA_BUILTIN_ALG_SHA_512) || \
-        defined(MBEDTLS_PSA_BUILTIN_ALG_SHA_384)
+        (defined(MBEDTLS_PSA_CRYPTO_CLIENT) && defined(PSA_WANT_ALG_SHA_512)) || \
+        defined(MBEDTLS_PSA_BUILTIN_ALG_SHA_384) || \
+        (defined(MBEDTLS_PSA_CRYPTO_CLIENT) && defined(PSA_WANT_ALG_SHA_384))
         mbedtls_sha512_context sha512;
 #endif
     } MBEDTLS_PRIVATE(ctx);
